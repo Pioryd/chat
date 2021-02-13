441efb24eec410d7fcb50137117e8d9aeb9b120e
Piotr Rydwański <pioryd123@gmail.com>
2021-02-13 18:29:31 +0100

    Add hook useWebSocket

---
+++ client/src/hooks/useWebSocket.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+export interface Packet {
+  packetId: string;
+  packetData: { [key: string]: any };
+}
+export default function useWebSocket() {
+  const [webSocket, setWebSocket] = React.useState<WebSocket>();
+  const [messages, setMessages] = React.useState<Packet[]>([]);
+  const [packetOnReconnect, setPacketOnReconnect] = React.useState<Packet>();
+
+  const pop = () => {
+    if (messages.length === 0) return [];
+    const returnMessages = [...messages];
+    setMessages([]);
+    return returnMessages;
+  };
+
+  const send = (packetId: string, packetData: any = {}) =>
+    webSocket && webSocket.send(JSON.stringify({ packetId, packetData }));
+
+  const connect = (packetOnConnected: Packet | undefined) => {
+    if (packetOnConnected == null) return;
+
+    const newWebSocket = new WebSocket(String(process.env.REACT_APP_WS_URL));
+    if (newWebSocket == null) return;
+
+    setPacketOnReconnect(packetOnConnected);
+
+    newWebSocket.onopen = () => {
+      console.log("Connected");
+
+      // state webSocket will be set after event "onopen"
+      newWebSocket.send(JSON.stringify(packetOnConnected));
+    };
+    newWebSocket.onmessage = (ev) =>
+      setMessages([...messages, JSON.parse(ev.data)]);
+    newWebSocket.onclose = () => {
+      console.log("Disconnected");
+      console.log("Reconnecting...");
+      connect(packetOnReconnect);
+    };
+
+    setWebSocket(newWebSocket);
+  };
+
+  const disconnect = () => setWebSocket(undefined);
+
+  React.useEffect(() => connect(packetOnReconnect), []);
+
+  return { messages, send, pop, connect, disconnect };
+}
