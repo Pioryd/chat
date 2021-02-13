4c863391ac647af0bd8bc32c78fd464e3311ed19
Piotr Rydwański <pioryd123@gmail.com>
2021-02-13 22:25:42 +0100

    Add server init files

--- client/src/components/Page/index.tsx
+++ client/src/components/Page/index.tsx
@@ -2,14 +2,28 @@ import React from "react";
 
 import { Container, Row, Col } from "react-bootstrap";
 
+import { AppContext, AppContextType } from "../../context/app";
+
 import MessagesList from "../MessagesList";
 import UsersList from "../UsersList";
 
 import "./index.scss";
 
 export default function Page() {
+  const { loggedIn, mainUser } = React.useContext(AppContext) as AppContextType;
+
+  if (!loggedIn)
+    return (
+      <div className="d-flex h-100">
+        <h6 className="mx-auto text-center justify-content-center align-self-center">
+          Log in.
+        </h6>
+      </div>
+    );
+
   return (
     <>
+      <p>{mainUser}</p>
       <div className="main-window">
         <Container className="border main-container">
           <Row>
