d50a8e3f757a66c8a7a0d2e6c943b5782bb77cd5
Piotr Rydwański <pioryd123@gmail.com>
2021-02-13 19:30:54 +0100

    Add component avatar

---
+++ client/src/components/LetterAvatar/index.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import { Tooltip, OverlayTrigger } from "react-bootstrap";
+
+import "./index.scss";
+
+export default function Avatar(props: {
+  name: string;
+  backgroundColor: string;
+  size: "big" | "small";
+  popover?: boolean;
+}) {
+  const [avatarClassName] = React.useState(`letter-avatar-${props.size}`);
+
+  return (
+    <div
+      className={avatarClassName}
+      style={{ backgroundColor: props.backgroundColor }}
+      aria-label={props.name}
+    >
+      <OverlayTrigger
+        overlay={
+          <Tooltip hidden={!props.popover} id="tooltip-disabled">
+            {props.name}
+          </Tooltip>
+        }
+      >
+        <span className="d-inline-block">
+          <div className="lettered-avatar">{props.name[0].toUpperCase()}</div>
+        </span>
+      </OverlayTrigger>
+    </div>
+  );
+}
